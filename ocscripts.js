eb8aaa73fb3de57bf447255d47ecff3c46ede85f
EmperorBob7 <40090388+EmperorBob7@users.noreply.github.com>
2023-12-21 00:18:20 -0600

    Operator Stat Char, TODO: Clipping & Stat Text & Total

--- ocscripts.js
+++ ocscripts.js
@@ -1,10 +1,11 @@
 let ctx, canvas;
-let trion, attack, defense, mobility, skill, range, command, tactics, reactions, strategy, specialtech, recon, attacker, shooter, sniper, gunner;
-const TriggerNames = ["opt", "shi", "sco", "egret", "aster", "handgun", "kog", "ray", "viper", "mete", "hound", "speark", "ibis", "light", "escudo", "arifle", "shot", "grenade", "free", "switchbox", "img", "blocked", "soget"];
-const TriggerUrls = ["https://cdn.discordapp.com/attachments/571784589882949692/588223959292706817/1560313908255.png", "https://cdn.discordapp.com/attachments/571784589882949692/588226241895858176/1560314451213.png", "https://cdn.discordapp.com/attachments/571784589882949692/588228644481269761/1560315030622.png", "https://cdn.discordapp.com/attachments/571784589882949692/591051692724977665/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591053193702539314/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591054144995983425/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591058852800233503/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591060849754374146/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591062402783772691/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591063097255526400/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591064834523660303/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591067006355374110/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595070798725906442/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595071264087998496/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595073834692902925/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595073561467682818/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595074859860492318/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595076936133246989/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/596874086341410826/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/596787826511577119/importantIcon.png", "./OcTemplate.png", "https://cdn.discordapp.com/attachments/571784589882949692/597287816635875338/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/610257477631606811/importantIcon.png"];
+let trion, attack, defense, mobility, skill, range, command, tactics;
+let mechanical, analysis, parallel;
+const TriggerNames = ["opt", "shi", "sco", "egret", "aster", "handgun", "kog", "ray", "viper", "mete", "hound", "speark", "ibis", "light", "escudo", "arifle", "shot", "grenade", "free", "switchbox", "img", "blocked", "soget", "opImg"];
+const TriggerUrls = ["https://cdn.discordapp.com/attachments/571784589882949692/588223959292706817/1560313908255.png", "https://cdn.discordapp.com/attachments/571784589882949692/588226241895858176/1560314451213.png", "https://cdn.discordapp.com/attachments/571784589882949692/588228644481269761/1560315030622.png", "https://cdn.discordapp.com/attachments/571784589882949692/591051692724977665/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591053193702539314/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591054144995983425/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591058852800233503/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591060849754374146/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591062402783772691/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591063097255526400/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591064834523660303/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/591067006355374110/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595070798725906442/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595071264087998496/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595073834692902925/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595073561467682818/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595074859860492318/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/595076936133246989/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/596874086341410826/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/596787826511577119/importantIcon.png", "./OcTemplate.png", "https://cdn.discordapp.com/attachments/571784589882949692/597287816635875338/importantIcon.png", "https://cdn.discordapp.com/attachments/571784589882949692/610257477631606811/importantIcon.png", "./Template_OP.png"];
 const TriggerImages = {};
 for (let i = 0; i < TriggerNames.length; i++) {
-    if (TriggerNames[i] === "soget") {
+    if (TriggerNames[i] === "opImg") {
         TriggerImages[TriggerNames[i]] = newImage(TriggerUrls[i], true);
     } else {
         TriggerImages[TriggerNames[i]] = newImage(TriggerUrls[i], false);
@@ -14,7 +15,6 @@ for (let i = 0; i < TriggerNames.length; i++) {
 let squadRank = "";
 const triggers = ["Free Trigger", "Free Trigger", "Free Trigger", "Free Trigger", "Free Trigger", "Free Trigger", "Free Trigger", "Free Trigger"];
 const orderProf = ["POSITION: ", "AGE: ", "BIRTHDAY: ", "HEIGHT: ", "BLOOD TYPE: ", "ZODIAC: ", "OCCUPATION: ", "LIKES: ", "FACTION: "];
-const orderStat = ["TRION: ", "MOBILITY: ", "REACTIONS: ", "RECON: ", "STRATEGY: ", "SPECIAL TECHNIQUES: "];
 const texts = ["", "DEFENSE TRIGGER", "OPTION TRIGGER", "ATTACKER TRIGGER", "ATTACKER TRIGGER", "ATTACKER TRIGGER", "ATTACKER TRIGGER", "SHOOTER TRIGGER", "SHOOTER TRIGGER", "SHOOTER TRIGGER", "SHOOTER TRIGGER", "SNIPER TRIGGER", "SNIPER TRIGGER", "SNIPER TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER", "DEFENSE TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER", "OPTION TRIGGER"];
 texts[54] = "TRAP TRIGGER";
 texts[55] = "OPTION TRIGGER";
@@ -26,6 +26,7 @@ texts[60] = "OPTION TRIGGER";
 const trigNames = ["", "SHIELD", "BAGWORM", "RAYGUST", "KOGETSU", "SCORPION", "KOGETSU: SPEAR", "ASTEROID", "HOUND", "METEORA", "VIPER", "EGRET", "IBIS", "LIGHTNING", "GEN'YO", "LEAD BULLET", "SENKU", "STARMAKER", "THRUSTER", "ESCUDO", "CHAMELEON", "DUMMY BEACON", "GRASSHOPPER", "SILENCER", "SPIDER", "TELEPORTER", "Assault Rifle: Viper", "Assault Rifle: Hound", "Assault Rifle: Asteroid", "Assault Rifle: Meteora", "Assault Rifle: Gimlet", "Assault Rifle: Salamander", "Assault Rifle: Tomahawk", "Grenade Gun: Viper", "Grenade Gun: Hound", "Grenade Gun: Asteroid", "Grenade Gun: Meteora", "Grenade Gun: Gimlet", "Grenade Gun: Salamander", "Grenade Gun: Tomahawk", "Shotgun: Viper", "Shotgun: Hound", "Shotgun: Asteroid", "Shotgun: Meteora", "Shotgun: Gimlet", "Shotgun: Salamander", "Shotgun: Tomahawk", "Handgun: Viper", "Handgun: Hound", "Handgun: Asteroid", "Handgun: Meteora", "Handgun: Gimlet", "Handgun: Salamander", "Handgun: Tomahawk", "SWITCHBOX", "BAGWORM TAG", "IDATEN", "GEIST", "CONNECTOR", "SOUGETSU", "FULL ARMS"];
 const trigImg = ["", TriggerImages.shi, TriggerImages.opt, TriggerImages.ray, TriggerImages.kog, TriggerImages.sco, TriggerImages.speark, TriggerImages.aster, TriggerImages.hound, TriggerImages.mete, TriggerImages.viper, TriggerImages.egret, TriggerImages.ibis, TriggerImages.light, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.escudo, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.arifle, TriggerImages.arifle, TriggerImages.arifle, TriggerImages.arifle, TriggerImages.arifle, TriggerImages.arifle, TriggerImages.arifle, TriggerImages.grenade, TriggerImages.grenade, TriggerImages.grenade, TriggerImages.grenade, TriggerImages.grenade, TriggerImages.grenade, TriggerImages.grenade, TriggerImages.shot, TriggerImages.shot, TriggerImages.shot, TriggerImages.shot, TriggerImages.shot, TriggerImages.shot, TriggerImages.shot, TriggerImages.handgun, TriggerImages.handgun, TriggerImages.handgun, TriggerImages.handgun, TriggerImages.handgun, TriggerImages.handgun, TriggerImages.handgun, TriggerImages.switchbox, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.opt, TriggerImages.soget, TriggerImages.opt];
 // 0-25 is regular 3-6 Attacker 7-10 Shooter 11-13 Sniper 14-18 Attachments 19-25 Extras 26-32 Assault Rifles 33-39 Grenade Launchers 40-46 Shotguns 47-53 Handguns
+let isOperator = false;
 
 function newImage(src, bool) {
     const newimg = new Image();
@@ -41,13 +42,22 @@ function newImage(src, bool) {
 function begin() {
     canvas = document.getElementById("cv");
     ctx = canvas.getContext("2d");
-    ctx.drawImage(TriggerImages.img, 0, 0, 1280, 720);
+    ctx.drawImage(TriggerImages.img, 0, 0, 1280, 590);
+    operatorCheck();
     draw();
     reset();
 }
 
 function reset() {
-    ctx.drawImage(TriggerImages.img, 0, 0, 1280, 720);
+    if (isOperator) {
+        ctx.drawImage(TriggerImages.opImg, 0, 0, 1280, 590);
+    } else {
+        ctx.drawImage(TriggerImages.img, 0, 0, 1280, 590);
+    }
+}
+
+function operatorCheck() {
+    isOperator = document.getElementById("operatorCheck").checked;
 }
 
 function getTrigs() {
@@ -80,14 +90,9 @@ function getParams() {
     range = Number(document.getElementById("range").value);
     command = Number(document.getElementById("command").value);
     tactics = Number(document.getElementById("tactics").value);
-    reactions = Number(document.getElementById("reactions").value);
-    strategy = Number(document.getElementById("strategy").value);
-    specialtech = Number(document.getElementById("specialtech").value);
-    recon = Number(document.getElementById("recon").value);
-    attacker = Number(document.getElementById("attacker").value);
-    shooter = Number(document.getElementById("shooter").value);
-    sniper = Number(document.getElementById("sniper").value);
-    gunner = Number(document.getElementById("gunner").value);
+    mechanical = Number(document.getElementById("mechanical").value);
+    analysis = Number(document.getElementById("analysis").value);
+    parallel = Number(document.getElementById("parallel").value);
 }
 
 /**
@@ -140,6 +145,60 @@ function getTactics(tr) {
     const ttx = 803 + (-9.4 * (tr - 1));
     const tty = 375 + (-9.15 * (tr - 1));
     return [ttx, tty];
+}
+
+function getOpTrion(tr) {
+    const tx = 812;
+    let ty = 384 + (-10.5 * tr);;
+    if(tr > 2) {
+        ty = 384 + (-12 * tr);
+    }
+    return [tx, ty];
+}
+
+function getMechanical(mr) {
+    let mx = 814 + (10 * mr);
+    let my = 385 + (-6 * mr);
+    if(mr > 2) {
+        mx = 814 + (10.5 * mr);
+    }
+    return [mx, my];
+}
+
+function getData(dr) {
+    let dx = 814 + (10 * dr);
+    let dy = 386 + (6 * dr);
+    if(dr > 2) {
+        dx = 814 + (10.5 * dr);
+    }
+    return [dx, dy];
+}
+
+function getParallel(pr) {
+    const px = 812;
+    let py = 388 + (11 * pr);
+    if(pr > 2) {
+        py = 388 + (12.25 * pr);
+    }
+    return [px, py];
+}
+
+function getOpTactics(tr) {
+    let ttx = 811 + (-10 * tr);
+    const tty = 386 + (6 * tr);
+    if(tr > 2) {
+        ttx = 811 + (-10.5 * tr);
+    }
+    return [ttx, tty];
+}
+
+function getOpCommand(cr) {
+    let cx = 811 + (-10 * cr);
+    const cy = 385 + (-6 * cr);
+    if(cr > 2) {
+        cx = 811 + (-10.5 * cr);
+    }
+    return [cx, cy];
 }
 
 function paramText() {
@@ -284,7 +343,7 @@ function getProfile() {
 }
 
 function drawProfile() {
-    let outY = 570;
+    let outY = 275;
     ctx.font = "normal normal normal 12px arial";
     const orderProf2 = getProfile();
     for (let i = 0; i <= 6; i++) {
@@ -322,59 +381,81 @@ function getLines(text, maxWidth) {
     lines.push(currentLine);
     return lines;
 }
-function drawStats() {
-    let outY = 275;
-    ctx.font = "normal normal normal 12px arial";
-    const orderStat2 = [trion, mobility, reactions, recon, strategy, specialtech];
-    if (parseInt(attacker) > 0 && parseInt(shooter) > 0 && parseInt(sniper) > 0 && parseInt(gunner) > 0) {
-        for (let i = 0; i <= 5; i++) {
-            ctx.fillText(orderStat[i] + orderStat2[i], 25, outY);
-            outY += 12;
-        }
-        if (parseInt(attacker) > 0) {
-            ctx.fillText("ATTACKER: " + attacker, 25, outY);
-            outY += 12;
-        }
-        if (parseInt(shooter) > 0) {
-            ctx.fillText("SHOOTER: " + shooter, 25, outY);
-            outY += 12;
-        }
-        if (parseInt(sniper) > 0) {
-            ctx.fillText("SNIPER: " + sniper, 25, outY);
-            outY += 12;
-        }
-        if (parseInt(gunner) > 0) {
-            ctx.fillText("GUNNER: " + gunner, 25, outY);
-            outY += 12;
-        }
-    } else {
-        for (let i = 0; i <= 5; i++) {
-            ctx.fillText(orderStat[i] + orderStat2[i], 25, outY);
-            outY += 14;
-        }
-        if (parseInt(attacker) > 0) {
-            ctx.fillText("ATTACKER: " + attacker, 25, outY);
-            outY += 14;
-        }
-        if (parseInt(shooter) > 0) {
-            ctx.fillText("SHOOTER: " + shooter, 25, outY);
-            outY += 14;
-        }
-        if (parseInt(sniper) > 0) {
-            ctx.fillText("SNIPER: " + sniper, 25, outY);
-            outY += 14;
-        }
-        if (parseInt(gunner) > 0) {
-            ctx.fillText("GUNNER: " + gunner, 25, outY);
-            outY += 14;
-        }
-    }
-}
+
+// function drawStats() {
+//     let outY = 275;
+//     ctx.font = "normal normal normal 12px arial";
+//     const orderStat2 = [trion, mobility, reactions, recon, strategy, specialtech];
+//     if (parseInt(attacker) > 0 && parseInt(shooter) > 0 && parseInt(sniper) > 0 && parseInt(gunner) > 0) {
+//         for (let i = 0; i <= 5; i++) {
+//             ctx.fillText(orderStat[i] + orderStat2[i], 25, outY);
+//             outY += 12;
+//         }
+//         if (parseInt(attacker) > 0) {
+//             ctx.fillText("ATTACKER: " + attacker, 25, outY);
+//             outY += 12;
+//         }
+//         if (parseInt(shooter) > 0) {
+//             ctx.fillText("SHOOTER: " + shooter, 25, outY);
+//             outY += 12;
+//         }
+//         if (parseInt(sniper) > 0) {
+//             ctx.fillText("SNIPER: " + sniper, 25, outY);
+//             outY += 12;
+//         }
+//         if (parseInt(gunner) > 0) {
+//             ctx.fillText("GUNNER: " + gunner, 25, outY);
+//             outY += 12;
+//         }
+//     } else {
+//         for (let i = 0; i <= 5; i++) {
+//             ctx.fillText(orderStat[i] + orderStat2[i], 25, outY);
+//             outY += 14;
+//         }
+//         if (parseInt(attacker) > 0) {
+//             ctx.fillText("ATTACKER: " + attacker, 25, outY);
+//             outY += 14;
+//         }
+//         if (parseInt(shooter) > 0) {
+//             ctx.fillText("SHOOTER: " + shooter, 25, outY);
+//             outY += 14;
+//         }
+//         if (parseInt(sniper) > 0) {
+//             ctx.fillText("SNIPER: " + sniper, 25, outY);
+//             outY += 14;
+//         }
+//         if (parseInt(gunner) > 0) {
+//             ctx.fillText("GUNNER: " + gunner, 25, outY);
+//             outY += 14;
+//         }
+//     }
+// }
+
 function drawRank() {
     ctx.font = "normal normal normal 37px stencil";
     ctx.fillStyle = "black";
     ctx.fillText(squadRank, 50, 63);
 }
+
+function clipStats() {
+    ctx.save();
+    ctx.beginPath();
+    if (isOperator) {
+
+    } else {
+        ctx.moveTo(812, 255); // Tri
+        ctx.lineTo(905, 291); // Atk
+        ctx.lineTo(944, 385); // Def
+        ctx.lineTo(905, 479); // Mob
+        ctx.lineTo(812, 518); // Skill
+        ctx.lineTo(718, 479); // Range
+        ctx.lineTo(680, 386); // Command
+        ctx.lineTo(719, 293); // Special
+        ctx.lineTo(812, 255); // Tri
+    }
+    // ctx.clip();
+}
+
 function draw() {
     reset();
     writeName();
@@ -388,44 +469,54 @@ function draw() {
     drawRank();
     writeRank();
     drawProfile();
-    drawStats();
+    // drawStats();
+    // Combatants
+    let tx, ty, ax, ay, dx, dy, mx, my, sx, sy, rx, ry, cx, cy, ttx, tty;
+    //Operators
+    let mox, moy, dax, day, ppx, ppy;
 
-    const [tx, ty] = getTrion(trion);
-    const [ax, ay] = getAttack(attack);
-    const [dx, dy] = getDefense(defense);
-    const [mx, my] = getMobility(mobility);
-    const [sx, sy] = getSkill(skill);
-    const [rx, ry] = getRange(range);
-    const [cx, cy] = getCommand(command);
-    const [ttx, tty] = getTactics(tactics);
-
-    ctx.save();
-    ctx.beginPath();
-    ctx.moveTo(812, 255); // Tri
-    ctx.lineTo(905, 291); // Atk
-    ctx.lineTo(944, 385); // Def
-    ctx.lineTo(905, 479); // Mob
-    ctx.lineTo(812, 518); // Skill
-    ctx.lineTo(718, 479); // Range
-    ctx.lineTo(680, 386); // Command
-    ctx.lineTo(719, 293); // Special
-    ctx.lineTo(812, 255); // Tri
-    ctx.clip();
+    if (isOperator) {
+        [tx, ty] = getOpTrion(trion);
+        [mox, moy] = getMechanical(mechanical);
+        [dax, day] = getData(analysis);
+        [ppx, ppy] = getParallel(parallel);
+        [ttx, tty] = getOpTactics(tactics);
+        [cx, cy] = getOpCommand(command);
+    } else {
+        [tx, ty] = getTrion(trion);
+        [ax, ay] = getAttack(attack);
+        [dx, dy] = getDefense(defense);
+        [mx, my] = getMobility(mobility);
+        [sx, sy] = getSkill(skill);
+        [rx, ry] = getRange(range);
+        [cx, cy] = getCommand(command);
+        [ttx, tty] = getTactics(tactics);
+    }
+    clipStats();
 
     ctx.beginPath();
     ctx.globalAlpha = 0.45;
     ctx.moveTo(tx, ty);
-    ctx.lineTo(ax, ay);
-    ctx.lineTo(dx, dy);
-    ctx.lineTo(mx, my);
-    ctx.lineTo(sx, sy);
-    ctx.lineTo(rx, ry);
-    ctx.lineTo(cx, cy);
-    ctx.lineTo(ttx, tty);
+    if (isOperator) {
+        ctx.lineTo(mox, moy);
+        ctx.lineTo(dax, day);
+        ctx.lineTo(ppx, ppy);
+        ctx.lineTo(ttx, tty);
+        ctx.lineTo(cx, cy);
+    } else {
+        ctx.lineTo(ax, ay);
+        ctx.lineTo(dx, dy);
+        ctx.lineTo(mx, my);
+        ctx.lineTo(sx, sy);
+        ctx.lineTo(rx, ry);
+        ctx.lineTo(cx, cy);
+        ctx.lineTo(ttx, tty);
+    }
     ctx.fill();
     ctx.restore();
     ctx.globalAlpha = 1;
 }
+
 function addTrigger() {
     let e = document.getElementById("customImg");
     let ret = parseInt(e.options[e.selectedIndex].value);
